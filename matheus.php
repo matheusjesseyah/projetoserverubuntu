aef89220c4022ccf8a92065f6767336b54d31c2f
Matheus <matheus.jesseyah@gmail.com>
2025-12-01 21:35:10 -0300

    index and php page add

---
+++ matheus.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página PHP</title>
+</head>
+<body>
+    <h1>Você está na página PHP!</h1>
+
+    <p>
+        👉 Clique abaixo para voltar à página inicial:
+    </p>
+
+    <a href="index.html">Voltar para o início</a>
+</body>
+</html>
